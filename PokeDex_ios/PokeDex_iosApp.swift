083a3219eaa889718191dd195990b4c2bc8c8de6
jordyAHI <jordy.yeoman@advancedhumanimaging.com>
2022-02-21 11:48:27 +0800

    Initial Commit

---
+++ PokeDex_ios/PokeDex_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDex_iosApp.swift
+//  PokeDex_ios
+//
+//  Created by Jordy Yeoman on 21/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokeDex_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
